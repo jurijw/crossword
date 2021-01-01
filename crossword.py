6a386d7df3ca09ef7fea0a868a1b13ffce932ecb
Jurij Waite <jurijlwaite@gmail.com>
2020-12-31 19:24:50 -0800

    Added first round of helper methods.

--- crossword.py
+++ crossword.py
@@ -1,6 +1,65 @@
 import numpy as np
 from user_input import hints_and_answers
 
-answers = list(hints_answers.values())
+answers = list(hints_and_answers.values())
+sorted_answers = list(reversed(sorted(answers, key=len)))
+longest_length = len(sorted_answers[0])
 
-print(answers)
+grid = np.zeros((longest_length, longest_length), dtype=str)
+
+# lists to track the start positions of already placed words
+# this ensures that only one word can be placed in one spot going
+# across and only one word can be placed in that spot going down
+across_placed = []
+down_placed = []
+
+
+# Helper functions
+def get_insert_spots(grid, word, insert_start_pos, left_to_right):
+    x, y = insert_start_pos
+    if left_to_right:
+        return grid[y][x:x+len(word)]
+    else:
+        return grid[:, x][y:y+len(word)]
+
+
+def word_goes_off_grid(grid, word, insert_start_pos, left_to_right):
+    x, y = insert_start_pos
+    if left_to_right:
+        return x + len(word) > grid.shape[0]
+    else:
+        return y + len(word) > grid.shape[1]
+
+
+def word_can_be_placed_at_pos(grid, word, insert_start_pos, left_to_right):
+    # Ensure that no other word has been placed at INSERT_START_POS with the same
+    # orientation. (e.g no two words go accross at (2,3))
+    if left_to_right and insert_start_pos in across_placed:
+        return False
+    if not left_to_right and insert_start_pos in down_placed:
+        return False
+    # Ensure the word does not go off the grid
+    if word_goes_off_grid(grid, word, insert_start_pos, left_to_right):
+        return False
+
+    x, y = insert_start_pos
+    # Get a the set of chars where the word would go
+    if left_to_right:
+        chars = grid[y][x:x+len(word)]
+    else:
+        chars = grid[y:y+len(word)][x]
+
+    # Compare all the words chars to the chars already on the grid. Only allow inserts
+    # if the corresponding spot on the grid is free or if the chars match
+    for word_char, grid_char in zip(word, chars):
+        if word_char != grid_char and grid_char:
+            return False
+    return True
+
+
+def place_word_at_pos(grid, word, insert_start_pos, left_to_right):
+    # Check that the placement is allowed
+    if not word_can_be_placed_at_pos(grid, word, insert_start_pos, left_to_right):
+        return False
+    else:
+        pass
