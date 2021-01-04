3aa356d7bbbc47dec99ac040fa8ec48a70128a5a
Jurij Waite <jurijlwaite@gmail.com>
2021-01-03 18:54:18 -0800

    Updated the print_grid function to more neatly layout the crossword. Fixed some issues with the generate function.

--- crossword.py
+++ crossword.py
@@ -25,10 +25,10 @@ def word_goes_off_grid(grid, word, insert_start_pos, left_to_right):
     if left_to_right:
         return x + len(word) > grid.shape[1]
     else:
-        return y + len(word) > grid.shape[4]
+        return y + len(word) > grid.shape[0]
 
 
-@np.vectorize
+# @np.vectorize
 def word_can_be_placed_at_pos(grid, word, insert_start_pos,
                               left_to_right, across_placed, down_placed):
     # Ensure that no other word has been placed at INSERT_START_POS with the same
@@ -69,16 +69,19 @@ def place_word_at_pos(grid, word, insert_start_pos,
                 grid[y][x + i] = char
                 # Record that the word has been placed
                 across_placed.append(insert_start_pos)
+            return True
         else:
             for j, char in enumerate(word):
                 grid[y + j][x] = char
                 down_placed.append(insert_start_pos)
+            True
 
 
 def get_grid_copy_with_word_at_pos(
         grid, word, insert_start_pos, left_to_right, across_placed, down_placed):
     # Create a grid copy
     grid_copy = np.copy(grid)
+    print_grid(grid_copy)
     # Place the word if possible and return the copy. If no placement is
     # possible return False
     if place_word_at_pos(grid_copy, word, insert_start_pos,
@@ -106,6 +109,12 @@ def get_intersection_placements(grid, word):
 def generate(grid, answers, across_placed=[], down_placed=[]):
     """Either modifies the existing grid by placing the answers into
     it in a crossword style or returns False if no crossword is possible"""
+
+    # Base case:
+    if len(answers) == 0:
+        return True
+
+    print_grid(grid)
     # np.random.shuffle(answers)
     # while len(answers) > 0:
     # Get a random word
@@ -113,6 +122,7 @@ def generate(grid, answers, across_placed=[], down_placed=[]):
         # np.random.shuffle(chars) TODO: ensure some randomness in choosingn the
         # intersection
         for i, char in enumerate(word):
+            print(word)
             occurences = np.where(grid == char)
             intersections = zip(occurences[0], occurences[1])
             for intersection in intersections:
@@ -128,23 +138,48 @@ def generate(grid, answers, across_placed=[], down_placed=[]):
                     new_grid = get_grid_copy_with_word_at_pos(
                         grid, word, insert_start_pos, left_to_right, across_placed, down_placed)
 
-                    if new_grid:
+                    print(
+                        f"word: {word}   intersection: {intersection}, left_to_right: {left_to_right}")
+                    pause = input("Press any key to continue: ")
+                    if new_grid != False:
+                        print(insert_start_pos)
                         # If the word was able to be placed recursively call the
                         # generate function with the new grid (with the word placed)
                         # and a new answer list (with the word removed)
                         new_answers = answers.copy()
                         new_answers.remove(word)
                         if generate(new_grid, new_answers):
-                            return new_grid
+                            grid = new_grid
+                            return True
     return False
 
 
-def main():
-    answers = map(clean_answers, hints_and_answers.values())
-    longest_answer_length = max(answers, key=len)
-    sorted_answers = sorted(answers, key=len)
+def print_grid(grid):
+    max_col_number = grid.shape[1]
+    max_row_number = grid.shape[0]
+    initial_column_spacing = len(str(max_row_number))
+    standard_column_spacing = len(str(max_col_number))
+    for _ in range(initial_column_spacing):
+        print(' ', end='')
+    print(' - ', end='')
+    for i in range(grid.shape[1]):
+        print(str(i).center(standard_column_spacing, ' '), end='')
+    print()
+    for j, row in enumerate(grid):
+        print(str(j).center(initial_column_spacing, ' '), end=' - ')
+        for spot in row:
+            if spot == '':
+                print('x'.center(standard_column_spacing, ' '), end='')
+            else:
+                print(spot.center(standard_column_spacing, ' '), end='')
+        print()
 
-    grid = np.zeros((longest_answer_length, longest_answer_length), dtype=str)
+
+def main():
+    answers = list(map(clean_answers, hints_and_answers.values()))
+    longest_answer_length = len(max(answers, key=len))
+    sorted_answers = sorted(answers, key=len, reverse=True)
+    grid = np.zeros((longest_answer_length + 25, longest_answer_length + 25), dtype=str)
 
     # Lists that store if words have been placed either across or down. This
     # is in order to prevent multiple words being placed at the same starting
@@ -155,12 +190,12 @@ def main():
     place_word_at_pos(
         grid,
         word=first_word,
-        insert_start_pos=(grid.shape[0] // 2, 0),
+        insert_start_pos=(12, grid.shape[0] // 2),
         left_to_right=True,
         across_placed=across_placed,
         down_placed=down_placed)
 
-    print(grid)
+    generate(grid, sorted_answers, across_placed, down_placed)
 
 
 if __name__ == "__main__":
